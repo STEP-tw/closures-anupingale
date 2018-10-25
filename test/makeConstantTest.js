fca7e3aef7e755520dd46cee93887f005a752634
Annapurna <44020027+anupingale@users.noreply.github.com>
2018-10-25 14:25:55 +0530

    Remove changes in testMakeConstant file.

--- test/makeConstantTest.js
+++ test/makeConstantTest.js
@@ -2,22 +2,10 @@ const assert=require("assert");
 const makeConstant = require('../src/lib.js').makeConstant;
 
 const testMakeConstantWithNoArgs = function() {
-  let constantlyReturnTwo = function(){
-    return makeConstant(2);
-  }
-
-  let constantlyReturnString = function() {
-    return makeConstant('hello');
-  }
-
-  let constantlyReturnArray = function() {
-    return makeConstant([1, 2, 3]);
-  }
-
-  let constantlyReturnObject = function() {
-    return makeConstant({a: 1, b: 2});
-  }
-
+  let constantlyReturnTwo = makeConstant(2);
+  let constantlyReturnString = makeConstant('hello');
+  let constantlyReturnArray = makeConstant([1, 2, 3]);
+  let constantlyReturnObject = makeConstant({a: 1, b: 2});
   assert.equal(2, constantlyReturnTwo());
   assert.equal('hello', constantlyReturnString());
   assert.deepEqual([1, 2, 3], constantlyReturnArray());
@@ -25,21 +13,10 @@ const testMakeConstantWithNoArgs = function() {
 };
 
 const testMakeConstantWithUselessArg = function() {
-  let constantlyReturnTwo = function(num) {
-    return makeConstant(2);
-  }
-
-  let constantlyReturnString = function() {
-    return makeConstant('hello');
-  }
-
-  let constantlyReturnArray = function() {
-    return makeConstant([1, 2, 3]);
-  }
-  let constantlyReturnObject = function() {
-    return makeConstant({a: 1, b: 2});
-  }
-
+  let constantlyReturnTwo = makeConstant(2);
+  let constantlyReturnString = makeConstant('hello');
+  let constantlyReturnArray = makeConstant([1, 2, 3]);
+  let constantlyReturnObject = makeConstant({a: 1, b: 2});
   assert.equal(2, constantlyReturnTwo(1));
   assert.equal('hello', constantlyReturnString(1));
   assert.deepEqual([1, 2, 3], constantlyReturnArray(1));
